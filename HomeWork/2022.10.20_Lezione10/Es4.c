4de97a8612c97972763efb546f0134a1363aae07
Te3sk <nicco.fulgaro@outloolk.it>
2022-11-11 12:31:41 +0100

    finish 22.10.20 homework :)

--- HomeWork/2022.10.20_Lezione10/Es4.c
+++ HomeWork/2022.10.20_Lezione10/Es4.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define ndip 4
+
 typedef struct {
     int matr;
     char name[20];
@@ -9,10 +11,10 @@ typedef struct {
     int cash;
 } Dipendente;
 
-void compute_cash ();
+void compute_cash (Dipendente*);
 
 int main () {
-    FILE* reg = fopen("Es4_input.dat", "rb");
+    FILE* reg = fopen("Es4_input2.dat", "rb");
 
     if (!reg) {
         perror("Error: ");
@@ -21,13 +23,31 @@ int main () {
 
     Dipendente dip[4];
     int c = 0;
-    char buffer[200];
+    char buffer[100];
 
-    for (int i = 0;i< 4; i++) {
-        // fscanf(reg, "%d %s %d %d %d\n", &dip[c].matr, &dip[c].name, &dip[c].mmass, &dip[c].aaass, &dip[c].cash);
-        // printf("-> %d %s %d %d %d\n", dip[c].matr, dip[c].name, dip[c].mmass, dip[c].aaass, dip[c].cash);
-        // c++;
-        fscanf(reg, "%[^\n]\n", &buffer);
-        printf("%s\n", buffer);
+    while(fread(&dip[c], sizeof(Dipendente), 1, reg)) {
+        c++;
+    }
+
+    printf("%-4s %-10s %-2s %-4s %-4s\n", "Matr", "Nome", "MM", "AA", "Stipendio");
+    for (int i = 0; i < ndip; i++) {
+        printf("%-4d %-10s %-2d %-4d %-4d\n", dip[i].matr, dip[i].name, dip[i].mmass, dip[i].aaass, dip[i].cash);
+    }
+
+    printf("%-4s %-10s %-4s\n", "Matr", "Nome", "Stipendio");
+    compute_cash(&dip[0]);
+
+    return 0;
+}
+
+void compute_cash(Dipendente* dip) {
+    for (int i = 0; i < ndip; ++i) {
+        if (dip[i].aaass < 2000) {
+            dip[i].cash = dip[i].cash + ((dip[i].cash * 22) / 100);
+            printf("%-4d %-10s %-4d\n", dip[i].matr, dip[i].name, dip[i].cash);
+        } else if(dip[i].aaass == 2000 && dip[i].mmass <= 5) {
+            dip[i].cash = dip[i].cash + ((dip[i].cash * 22) / 100);
+            printf("%-4d %-10s %-4d\n", dip[i].matr, dip[i].name, dip[i].cash);
+        }
     }
 }
